6a0d8375ba8244166798688434f8c31b82aa82bd
Flávio Florentino <florentino.flavioweb@gmail.com>
2021-01-26 12:03:56 -0300

    fix: added small code fixes

--- src/app/dashboard/entities/services/dashboard-data.service.ts
+++ src/app/dashboard/entities/services/dashboard-data.service.ts
@@ -1,12 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { DefaultDataService, HttpUrlGenerator } from '@ngrx/data';
-import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class DashboardDataService extends DefaultDataService<any> {
-
   constructor(http: HttpClient, httpUrlGenerator: HttpUrlGenerator) {
     super('Dashboard', http, httpUrlGenerator);
+  }
+
+  getAll() {
+    return this.http
+      .get(environment.dashboardApi)
+      .pipe(map((res) => res['payload']));
   }
 }
--- src/app/store/app-store/entity/entity.module.ts
+++ src/app/store/app-store/entity/entity.module.ts
@@ -1,41 +1,33 @@
 import { NgModule } from '@angular/core';
-import { EntityDataModule, EntityServices, EntityDataService, DefaultDataServiceConfig } from '@ngrx/data'
-import { entityMetadata } from './entity-metadata';
-import { AppEntityServices } from './app-entity-services';
+import {
+  DefaultDataServiceConfig,
+  EntityDataModule,
+  EntityDataService,
+  EntityServices,
+} from '@ngrx/data';
 import { DashboardDataService } from 'src/app/dashboard/entities/services/dashboard-data.service';
 import { DashboardEntityService } from 'src/app/dashboard/entities/services/dashboard-entity.service';
-
-const defaultDataServiceConfig: DefaultDataServiceConfig = {
-  root: 'http://localhost:3000/',
-  entityHttpResourceUrls: {
-    Dashboard: {
-      entityResourceUrl: 'api/dashboard',
-      collectionResourceUrl: 'api/dashboard'
-    }
-  },
-
-  timeout: 3000,
-};
+import { AppEntityServices } from './app-entity-services';
+import { entityMetadata } from './entity-metadata';
 
 @NgModule({
   declarations: [],
   imports: [
     EntityDataModule.forRoot({
       entityMetadata: entityMetadata,
-    })
+    }),
   ],
   providers: [
     AppEntityServices,
-    { provide: DefaultDataServiceConfig, useValue: defaultDataServiceConfig },
     { provide: EntityServices, useExisting: AppEntityServices },
     DashboardDataService,
     DashboardEntityService,
-  ]
+  ],
 })
 export class EntityModule {
   constructor(
     entityDataService: EntityDataService,
-    dashboardDataService: DashboardDataService,
+    dashboardDataService: DashboardDataService
   ) {
     entityDataService.registerService('Dashboard', dashboardDataService);
   }
